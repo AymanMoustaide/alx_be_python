e411b232432c460be65328323c8e5fb48559d115
AymanMoustaide <ayman.moustaide@icloud.com>
2024-06-26 06:04:11 +0100

    :

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,11 @@
 
-number = int(input("Enter a number to see its multiplication table: "))
+number = int(input('enter a number to see its multiplication table:'))
 
-for x in range(1, 11):
-    result = number * x
-    print(f"{number} * {x} = {result}")
+for i in range(1, 11):
+    y = i
+    x = number 
+    z = number * i 
+
+    print(f"{x} * {y} = {z}")
+    
+
