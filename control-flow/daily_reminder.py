ee814bfc2fc4047afb37ac15019312c99963e58d
AymanMoustaide <ayman.moustaide@icloud.com>
2024-06-26 06:25:41 +0100

    Personal Daily Reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,82 +1,40 @@
 
 
 # #* Prompt for a Single Task:
-    
-# #! ASK THE USER TO INPUT A TASK DESCRIPTION AND SAVE IT INTO A TASK VARIABLE
-# task = input('TASK DESCRIPTION:')
 
-# # ! PROMPT FOR THE TASK’S PRIORITY (HIGH, MEDIUM, LOW) AND SAVE IT INTO A PRIORITY VARIABLE
-# priority = input('TASK PRIORITY:')
+#! ASK THE USER TO INPUT A TASK DESCRIPTION AND SAVE IT INTO A TASK VARIABLE
+task = input("Enter your task:")
 
-# #! IN A TIME_BOUND VARIABLE, ASK IF THE TASK IS TIME-BOUND (YES OR NO)
-# time_bound = input('IS IT TIME-BOUND? (YES/NO):')
+#! PROMPT FOR THE TASK’S PRIORITY (HIGH, MEDIUM, LOW)
+priority = input("Priority (high/medium/low):")
 
+#! IN A TIME_BOUND VARIABLE, ASK IF THE TASK IS TIME-BOUND (YES OR NO)
+time_bound = input("Is it time-bound? (yes/no):")
 
-# #* Process the Task Based on Priority and Time Sensitivity:
+#//Add
+error_message = "something went wrong!"
+first_part = f"{task} is a {priority} priority task"
 
-# #! Use a Match Case statement to react differently based on the task’s priority.
+#! USE A MATCH CASE STATEMENT TO REACT DIFFERENTLY BASED ON THE TASK’S PRIORITY.
+match priority:
+    case "high":
+        reminder = "that requires immediate attention today!"
+    case "medium":
+       reminder = "schedule it for later"
+    case "low":
+        reminder = "Consider completing it when you have free time."
+    case _ :
+        message = error_message
 
-# match priority:
-#     case 'high':
-#         print(f'Reminder:, {task} is a high priority task that requires immediate attention today!')
-#     case 'medium':
-#         print('This is a medium-priority task.')
-#     case 'low':
-#         print('This is a low-priority task.')
+#! WITHIN THE MATCH CASE OR AFTER, USE AN IF STATEMENT TO MODIFY THE REMINDER IF THE TASK IS TIME-BOUND.
+if time_bound == "yes":
+    reminder = "that requires immediate attention today!"
+elif time_bound == "no":
+    reminder = "Consider completing it when you have free time."
+else:
+    message = error_message 
 
-
-# #! Within the Match Case or after, use an if statement to modify the reminder if the task is time-bound.
-
-
-# num_list = [33,42,5,66,77,22,16,79,36,62,78,43,88,39,53,67,89,11]
-
-# count = 0
-# for i, v in enumerate(num_list):
-#     if v <= 50:
-#         print(f'above "50"',v, i)
-
-# print()
-
-# for i, v in enumerate(num_list):
-#     if i <= 49:
-#         print(f'under 49',{v}, {i})
-
-
-
-
-
-
-
-#_ You are going to write a program that calculates the highest score from a List of scores.
-
-# student_scores = [78, 65, 89, 86, 55, 91, 64, 89]
-
-# for n in range(0, len(student_scores)):
-#     print(n)
-
-# student_scores[n] = int(student_scores[n])
-
-
-
-# student_scores.sort()
-
-
-# print(f'The highest score in the class is:', student_scores [-1])
-
-
-
-#// adj = ["red", "big", "tasty"]
-#// fruits = ["apple", "banana", "cherry"]
-
-# //print('Choise')
-# //for x in adj:
-#  // for y in fruits:
-#    // print(x, y)
-
-#     //if x == adj[0] and y == fruits[2]:
-#       //print()
-#       print('Choise')
-#     //elif x == adj[1] and y == fruits[2]:
-#       //print()
-#       //print('Choise')
-
+message = (f"Reminder: '{task}' is a {priority} priority task. that requires immediate attention today!" 
+           if time_bound == "yes" else "Consider completing it when you have free time.")
+print("Reminder: ", reminder)
+print(message)
