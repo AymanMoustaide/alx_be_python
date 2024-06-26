82807f5abbdb256b3d4359367554bcbd1340dbcd
AymanMoustaide <ayman.moustaide@icloud.com>
2024-06-26 12:03:33 +0100

    Challenge guessing game

--- control-flow/guess_the_number.py
+++ control-flow/guess_the_number.py
@@ -1,81 +1,65 @@
 import random
 
-def guess_the_number():
-    #! GENERATE A SECRET NUMBER BETWEEN 1 AND 10
-    secret_number = random.randint(1, 10)
-
-    print("I'M THINKING OF A NUMBER BETWEEN 1 AND 10. CAN YOU GUESS IT?")
-
-    # Initialize the counter for the number of guesses
-    guess_count = 0
-
-    while True:
-        # Get user's guess and convert it to an integer
-        # try:
-        guess = int(input("ENTER YOUR GUESS: "))
-        # except ValueError:
-        # print("PLEASE ENTER A VALID INTEGER.")
-            # continue  # Ask for a new guess if input is not a valid integer
-
-        #! INCREMENT THE GUESS COUNTER
-        guess_count += 1
-
-        #! MATCH THE USER'S GUESS WITH THE SECRET NUMBER
-        match guess:
-            case _ if guess == secret_number:
-                print(f"CONGRATULATIONS, YOU GUESSED IT IN {guess_count} TRIES!")
-                break  # Exit the loop if the guess is correct
-            case _ if guess > secret_number:
-                print("OOPS, YOUR GUESS IS A BIT HIGH. TRY AGAIN!")
-            case _ if guess < secret_number:
-                print("NOPE, YOUR GUESS IS A BIT LOW. GIVE IT ANOTHER SHOT!")
-
-    # Ask the user if they want to play again
-    play_again = input("PLAY AGAIN? (YES/NO): ").lower()
-
-    if play_again == "yes":
-        guess_the_number()  # Restart the game
-    else:
-        print("THANKS FOR PLAYING! GOODBYE!")
-
-# Call the function to start the game
-guess_the_number()
-
 # def guess_the_number():
+#     #! GENERATE A SECRET NUMBER BETWEEN 1 AND 10
+#     secret_number = random.randint(1, 10)
 
-#     secret_number = random.randint(1, 2) #! Generate a secret number between 1 and 10
-    
 #     print("I'M THINKING OF A NUMBER BETWEEN 1 AND 10. CAN YOU GUESS IT?")
-    
+
+#     #! INITIALIZE THE COUNTER FOR THE NUMBER OF GUESSES
 #     guess_count = 0
 
 #     while True:
-#         try:
-#             guess = int(input("ENTER YOUR GUESS: "))
-#         finally:
-#             print("PLEASE ENTER A VALID INTEGER.")
-            
-
+#         guess = int(input("ENTER YOUR GUESS: "))
+    
+#         #! INCREMENT THE GUESS COUNTER
 #         guess_count += 1
 
+#         #! MATCH THE USER'S GUESS WITH THE SECRET NUMBER
 #         match guess:
 #             case _ if guess == secret_number:
 #                 print(f"CONGRATULATIONS, YOU GUESSED IT IN {guess_count} TRIES!")
 #                 break  #! EXIT THE LOOP IF THE GUESS IS CORRECT
-
 #             case _ if guess > secret_number:
-#                 print("Oops, your guess is a bit high. Try again!")
+#                 print("OOPS, YOUR GUESS IS A BIT HIGH. TRY AGAIN!")
 #             case _ if guess < secret_number:
 #                 print("NOPE, YOUR GUESS IS A BIT LOW. GIVE IT ANOTHER SHOT!")
 
-#     #! PLAY AGAIN
+#     #! PLAY AGAIN ??? 
 #     play_again = input("PLAY AGAIN? (YES/NO): ").lower()
 
-#     if play_again == "YES":
-#         guess_the_number()  #! RESTART THE GAME WITH THE WHILE LOOP
+#     if play_again == "yes":
+#         guess_the_number()  #! RESTART THE GAME
 #     else:
 #         print("THANKS FOR PLAYING! GOODBYE!")
 
-
-# #! CALL THE FUNCTION
+# #! START THE GAME
 # guess_the_number()
+
+
+#_ print("Guessing Game!")
+
+while True:  #! LOOP TO ALLOW REPLAYING THE GAME
+    secret_number = random.randint(1, 10)
+    guess_count = 0  #! Counter
+    print("\nI'm thinking of a number between 1 and 10. Can you guess it?")
+    
+    while True:
+        guess = int(input("Enter your guess: "))
+        
+        guess_count += 1  #! INCREMENT THE GUESS COUNTER
+        
+        match guess:
+            case _ if guess == secret_number:
+                print(f"Congratulations, you guessed it in {guess_count} tries!")
+                break #! IF CORRECT QUIT TO RESTART
+            case _ if guess > secret_number:
+                print("Oops, your guess is a bit high. Try again!")
+            case _ if guess < secret_number:
+                print("Nope, your guess is a bit low. Give it another shot!")
+    
+    #! PLAY AGAIN !!! 
+    play_again = input("Play again? (yes/no): ")
+    if play_again != 'yes':
+        print("Thanks for playing! Goodbye!")
+        break
