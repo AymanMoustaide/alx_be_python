937a2d5ff055fc48e3c8da2b9c23e35bb02405f6
AymanMoustaide <ayman.moustaide@icloud.com>
2024-06-26 05:18:55 +0100

    Multiplication Table Generator

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -8,5 +8,5 @@ x = int(input('ENTER A NUMBER TO SEE ITS MULTIPLICATION TABLE:'))
 #! USE A FOR LOOP TO ITERATE THROUGH THE NUMBERS 1 TO 10.
 
 for y in range(1, 11):
-    z = x * y #! For each iteration, calculate the product of the user’s number and the iterator (the current number in the loop from 1 to 10).
-    print(x, '*', y, '=', z) #! Print each line of the multiplication table in the format: “X * Y = Z”, where X is the user’s number, Y is the current number in the loop, and Z is the product.
+    z = x * y 
+    print(x, '*', y, '=', z)
