f247856180c3b60d9ec72445f6d15fed272e9b18
AymanMoustaide <ayman.moustaide@icloud.com>
2024-06-26 05:32:28 +0100

    Multiplication Table Generator

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,8 +1,17 @@
 
 #_OBJECTIVE: USE A FOR LOOP TO GENERATE AND PRINT THE MULTIPLICATION TABLE FOR A GIVEN NUMBER.
 
-X = int(input('ENTER A NUMBER TO SEE ITS MULTIPLICATION TABLE:'))
+x = int(input('ENTER A NUMBER TO SEE ITS MULTIPLICATION TABLE:'))
 
-for Y in range(1, 11):
-    Z = X * Y 
-    print(X, '*', Y, '=', Z)
+for y in range(1, 11):
+    z = x * y
+
+
+    print(f'{x} * {y} = {z}')
+    
+
+
+
+
+    # Z = X * Y 
+    # print(X, '*', Y, '=', Z)
