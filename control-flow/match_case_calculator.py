bba892cac22475fa71f159b301f7d6155178befa
AymanMoustaide <ayman.moustaide@icloud.com>
2024-06-26 02:53:47 +0100

    :

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,18 +1,6 @@
-
-
-#_ OBJECTIVE: LEARN TO USE MATCH CASE STATEMENTS FOR HANDLING MULTIPLE OPERATIONS IN A SIMPLE CALCULATOR PROGRAM.
-
-
-#_  DEVELOP A PYTHON SCRIPT NAMED MATCH_CASE_CALCULATOR.PY. THIS CALCULATOR WILL PROMPT THE USER TO ENTER TWO NUMBERS AND SELECT AN OPERATION 
-#_ (ADDITION, SUBTRACTION, MULTIPLICATION, OR DIVISION). THE SCRIPT WILL THEN PERFORM THE SELECTED OPERATION USING A MATCH CASE STATEMENT AND DISPLAY THE RESULT.
-
-#! INSTRUCTIONS:
-
-#// MAKE SURE YOU USE NUM1 AND NUM2 FOR FIRST AND SECOND NUMBERS
-
-num1 = int(input('enter the first number:'))  #! ASK THE USER TO INPUT TWO NUMBERS (ONE AT A TIME) USING: “ENTER THE FIRST NUMBER: ” AND “ENTER THE SECOND NUMBER: ”.
-num2 = int(input('enter the second number:'))
-operator = input('choose the operation (+, -, *, /):') #! ASK FOR THE TYPE OF OPERATION THEY’D LIKE TO PERFORM: “CHOOSE THE OPERATION (+, -, *, /): ”.
+num1 = int(input('Enter the first number:'))  
+num2 = int(input('Enter the second number:'))
+operator = input('Ehoose the operation (+, -, *, /):') 
 
 match operator: 
     case '+':
@@ -30,29 +18,5 @@ match operator:
             print(f'the result is', result)
         else:
             print('cannot divide by zero')
-            
-    
-
-
-    
-
-#*FOR ADDITION (+), SUBTRACT (-), MULTIPLY (*), AND DIVIDE (/).
-
-#* DISPLAY THE RESULT OF THE OPERATION IN A USER-FRIENDLY MESSAGE, E.G., “THE RESULT IS [RESULT].”
-
-
-#_ EXAMPLE INTERACTION:
-
-# ENTER THE FIRST NUMBER: 10
-# ENTER THE SECOND NUMBER: 5
-# CHOOSE THE OPERATION (+, -, *, /): *
-# THE RESULT IS 50.
-
-#* OR, FOR A DIVISION BY ZERO SCENARIO:
-
-# ENTER THE FIRST NUMBER: 10
-# ENTER THE SECOND NUMBER: 0
-# CHOOSE THE OPERATION (+, -, *, /): /
-# CANNOT DIVIDE BY ZERO.
-
-
+    case _ :
+        print('unkown operation')
