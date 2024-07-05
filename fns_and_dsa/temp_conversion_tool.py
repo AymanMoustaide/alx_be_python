4b173c5bf87b7582ce50311131afd654acce3b26
AymanMoustaide <ayman.moustaide@icloud.com>
2024-07-05 14:16:51 +0100

    Temperature Conversion Tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -6,22 +6,23 @@
 
 #! F To C = (14°F − 32) × 5/9 = -10°C
 
-FAHRENHEIT_TO_CELSIUS_FACTOR = ''
-CELSIUS_TO_FAHRENHEIT_FACTOR = ''
-
 temperature = int(input('Enter the temperature to convert:'))
+
+FAHRENHEIT_TO_CELSIUS_FACTOR = (temperature - 32) * 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = (temperature * 9/5) + 32
+
 question = input('Is this temperature in Celsius or Fahrenheit? (C/F):')
 
 
 def convert_to_celsius():
     global FAHRENHEIT_TO_CELSIUS_FACTOR
-    FAHRENHEIT_TO_CELSIUS_FACTOR = (temperature - 32) * 5 / 9
+    # FAHRENHEIT_TO_CELSIUS_FACTOR = (temperature - 32) * 5 / 9
     print (f'{temperature}°F is {FAHRENHEIT_TO_CELSIUS_FACTOR}°C')
 # convert_to_celsius()
 
 def convert_to_fahrenheit():
     global CELSIUS_TO_FAHRENHEIT_FACTOR
-    CELSIUS_TO_FAHRENHEIT_FACTOR = (temperature * 9/5) + 32
+    # CELSIUS_TO_FAHRENHEIT_FACTOR = (temperature * 9/5) + 32
     print(f'{temperature}°C is {CELSIUS_TO_FAHRENHEIT_FACTOR}°F')
 # convert_to_fahrenheit()
 
