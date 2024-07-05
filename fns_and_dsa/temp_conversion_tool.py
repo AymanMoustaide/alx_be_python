0a73789b32fb265493fb4233aa60d6e3fc68d5cf
AymanMoustaide <ayman.moustaide@icloud.com>
2024-07-05 14:21:15 +0100

    dd

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,29 +1,19 @@
-
-
-
-
-#! C TO F = (14°C × 9/5) + 32 = 57.2°F
-
-#! F To C = (14°F − 32) × 5/9 = -10°C
-
-# temperature = int(input('Enter the temperature to convert:'))
-
-FAHRENHEIT_TO_CELSIUS_FACTOR = (temperature - 32) * 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = (temperature * 9/5) + 32
+fahrenheit_to_celsius_factor = (temperature - 32) * 5 / 9
+celsius_to_fahrenheit_factor = (temperature * 9/5) + 32
 
 question = input('Is this temperature in Celsius or Fahrenheit? (C/F):')
 
 
 def convert_to_celsius():
-    global FAHRENHEIT_TO_CELSIUS_FACTOR
-    # FAHRENHEIT_TO_CELSIUS_FACTOR = (temperature - 32) * 5 / 9
-    print (f'{temperature}°F is {FAHRENHEIT_TO_CELSIUS_FACTOR}°C')
+    global fahrenheit_to_celsius_factor
+    # fahrenheit_to_celsius_factor = (temperature - 32) * 5 / 9
+    print (f'{temperature}°F is {fahrenheit_to_celsius_factor}°C')
 # convert_to_celsius()
 
 def convert_to_fahrenheit():
-    global CELSIUS_TO_FAHRENHEIT_FACTOR
-    # CELSIUS_TO_FAHRENHEIT_FACTOR = (temperature * 9/5) + 32
-    print(f'{temperature}°C is {CELSIUS_TO_FAHRENHEIT_FACTOR}°F')
+    global celsius_to_fahrenheit_factor
+    # celsius_to_fahrenheit_factor = (temperature * 9/5) + 32
+    print(f'{temperature}°C is {celsius_to_fahrenheit_factor}°F')
 # convert_to_fahrenheit()
 
 if question == 'C':
