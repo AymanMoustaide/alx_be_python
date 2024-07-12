3454167d56d30979bf92b5d3cfd645f0fd4c3fb6
AymanMoustaide <ayman.moustaide@icloud.com>
2024-07-12 11:04:59 +0100

    :

--- programming_paradigm/Simple_Calculator.py
+++ programming_paradigm/Simple_Calculator.py
@@ -1,4 +1,4 @@
-from test_simple_calculator import TestSimpleCalculator
+# simple_calculator.py
 
 class SimpleCalculator:
     """A simple calculator class that supports basic arithmetic operations."""
