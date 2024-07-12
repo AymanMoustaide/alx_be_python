6f67d4f0e96458eacc94b4d8aaea1b4d0d56fe55
AymanMoustaide <ayman.moustaide@icloud.com>
2024-07-12 10:50:19 +0100

    Robust Division Calculator with Command Line Arguments

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -5,7 +5,7 @@ def safe_divide(numerator, denominator):
         den = float(denominator)
 
         result = num / den
-        return result
+        return f"The result of the division is {result:.1f}"
     
     except ZeroDivisionError:
         return 'Error: Cannot divide by zero.'
