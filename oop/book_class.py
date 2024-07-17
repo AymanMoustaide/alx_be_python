d00a1572aabfd975c7909ac3fef57ea79f5134b0
AymanMoustaide <ayman.moustaide@icloud.com>
2024-07-18 00:28:59 +0100

    Dive into Python Magic Methods

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,5 +1,6 @@
 
-class Book():
+
+class Book:
     def __init__(self, title, author, year):
         self.title = title
         self.author = author
@@ -13,5 +14,3 @@ class Book():
     
     def __repr__(self):
         return f'Book{self.title}, {self.author}, {self.year})'
-
-        
