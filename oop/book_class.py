1eb3d08d837bb4ed1bc6ed622566f55e5aa9144d
AymanMoustaide <ayman.moustaide@icloud.com>
2024-07-18 00:34:16 +0100

    Dive into Python Magic Methods

--- oop/book_class.py
+++ oop/book_class.py
@@ -5,6 +5,7 @@ class Book:
         self.title = title
         self.author = author
         self.year = year
+        
 
     def __del__(self):
         print(f'Deleting {self.title}')
@@ -13,6 +14,4 @@ class Book:
         return f'{self.title} by {self.author}, published in {self.year}'
     
     def __repr__(self):
-        return f'Book {self.title}, {self.author}, {self.year})'
-    
-    
+        return f'Book {self.title}, {self.author}, {self.year}'
