6bb3ba3dc65a854834511c4ff9537c1ecd712af2
AymanMoustaide <ayman.moustaide@icloud.com>
2024-07-18 23:49:07 +0100

    :

---
+++ hh/polymorphism_demo.py
@@ -0,0 +1,27 @@
+import math
+
+class Shape:
+    def area(self): 
+        raise NotImplementedError
+    
+    def description(self):
+        return "This is a shape"
+    
+    
+class Rectangle(Shape):
+    def __init__(self, length ,width):
+
+        self.width = width
+        self.length  = length 
+        
+    def area(self):
+        return self.length * self.width
+    
+    
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+        
+        
+    def area(self):
+        return math.pi * self.radius ** 2
